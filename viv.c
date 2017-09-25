22597d673f5f846f270516a832ac55bb8c131f44
vivekvi <30343467+vivekvi@users.noreply.github.com>
2017-09-25 14:57:59 +0530

    Update viv.c

--- viv.c
+++ viv.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    double number;
+    int number;
 
     printf("Enter a number: ");
-    scanf("%lf", &number);
+    scanf("%d", &number);
 
-    if (number <= 0.0)
+    if (number <= 0)
     {
-        if (number == 0.0)
-            printf("You entered 0.");
+        if (number == 0)
+            printf("You entered");
         else
-            printf("You entered a negative number.");
+            printf("You entered a negative number");
     }
     else
-        printf("You entered a positive number.");
+        printf("You entered a positive number");
     return 0;
 }
